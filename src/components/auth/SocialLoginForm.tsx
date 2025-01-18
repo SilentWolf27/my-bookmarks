fcf5ddf0d4ce63c4f22a0267278a4349323f54a2
Pedro David <97pedro@live.com.mx>
2025-01-18 10:11:44 -0600

    Add SocialLoginForm component; implement a reusable form for social login buttons

---
+++ src/components/auth/SocialLoginForm.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  action?: () => void;
+}
+
+export function SocialLoginForm({ children }: Props) {
+  return (
+    <form>
+      <button
+        type="submit"
+        className="w-100  flex justify-center items-center gap-2 border border-zinc-300 rounded-md py-2 px-4">
+        {children}
+      </button>
+    </form>
+  );
+}
