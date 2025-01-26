daa88d0f1c6a4940cfed801db19d3481be99495c
Pedro David <97pedro@live.com.mx>
2025-01-26 11:15:38 -0600

    Refactor SideBar component to remove authentication path check and simplify layout

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -1,19 +1,9 @@
-"use client";
-
 import { signOut } from "@/actions/auth/signOut";
 import { LogoutOutlined, PlusOutlined } from "@ant-design/icons";
-import { usePathname } from "next/navigation";
 
 export function SideBar() {
-  const pathame = usePathname();
-
-  const isAuthPath = pathame.includes("/auth");
-
   return (
-    <aside
-      className={`${
-        isAuthPath ? "hidden" : "block"
-      } min-w-[300px] w-[18%] max-w-[420px] px-6 py-8 bg-background text-primary-font flex flex-col justify-between`}>
+    <aside className="min-w-[300px] w-[18%] max-w-[420px] px-6 py-8 bg-background text-primary-font flex flex-col justify-between">
       <article>
         <div className="flex justify-between items-center">
           <h1 className="text-left font-bold text-lg">My Bookmarks</h1>
