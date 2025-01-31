7cd71e2c66be35f90794b1693ddf3b82b98f1fbd
Pedro David <97pedro@live.com.mx>
2025-01-31 07:20:40 -0600

    Refactor ModalHeader to accept className prop and remove unused ItemType component

--- src/components/common/Modal/ModalHeader.tsx
+++ src/components/common/Modal/ModalHeader.tsx
@@ -4,14 +4,15 @@ import { useRouter } from "next/navigation";
 interface Props {
   children?: React.ReactNode;
   onClose?: () => void;
+  className?: string;
 }
 
-export default function ModalHeader({ children, onClose }: Props) {
+export default function ModalHeader({ children, onClose, className }: Props) {
   return (
     <div
       className={`flex items-center h-auto ${
         children ? "justify-between" : "justify-end"
-      }`}>
+      } ${className}`}>
       {children}
       <button type="button" onClick={onClose}>
         <CloseOutlined />
