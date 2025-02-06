213ff1d777fc5e368f00c7d8e877441b9b7aab11
Pedro David <97pedro@live.com.mx>
2025-02-06 08:54:52 -0600

    Enhance CollectionNavItem: add toggle functionality for dropdown and update icon state based on open/close status

--- src/collections/components/CollectionNavItem.tsx
+++ src/collections/components/CollectionNavItem.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 import { Collection } from "../interfaces/Collections";
 import {
   CaretDownOutlined,
-  DownOutlined,
+  CaretUpOutlined,
   FolderOpenOutlined,
   FolderOutlined,
-  MoreOutlined,
 } from "@ant-design/icons";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 interface Props {
   collection: Collection;
@@ -17,6 +17,7 @@ interface Props {
 
 export default function CollectionNavItem({ collection }: Props) {
   const currentPath = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
 
   const isActive = currentPath.startsWith(`/colecciones/${collection.id}`);
   return (
@@ -34,8 +35,10 @@ export default function CollectionNavItem({ collection }: Props) {
         )}
         {collection.name}
       </Link>
-      <button className="opacity-0 group-hover:opacity-100 transition-opacity duration-350 ease-in-out py-1 px-3">
-        <CaretDownOutlined className="text-sm" />
+      <button
+        className="opacity-0 group-hover:opacity-100 transition-opacity duration-350 ease-in-out py-1 px-3 text-sm"
+        onClick={() => setIsOpen((prev) => !prev)}>
+        {isOpen ? <CaretUpOutlined /> : <CaretDownOutlined />}
       </button>
     </li>
   );
