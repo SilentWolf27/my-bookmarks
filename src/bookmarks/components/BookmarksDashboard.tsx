bb1630972db06801854e7f8ea767f667d3004368
Pedro David <97pedro@live.com.mx>
2025-02-10 07:50:14 -0600

    Add FastCreateBookmarkButton to BookmarksDashboard

--- src/bookmarks/components/BookmarksDashboard.tsx
+++ src/bookmarks/components/BookmarksDashboard.tsx
@@ -2,6 +2,7 @@
 
 import Searchbar from "@/components/Searchbar/Searchbars";
 import { Bookmark } from "../interfaces";
+import FastCreateBookmarkButton from "./FastCreateBookmarkButton";
 
 interface Props {
   bookmarks: Bookmark[];
@@ -13,12 +14,14 @@ export default function BookmarksDashboard({ bookmarks }: Props) {
   };
 
   return (
-    <div className="flex justify-end items-center gap-2">
-      <Searchbar
-        className="w-[300px] py-1 px-3 rounded-md bg-white"
-        onChange={handleSearch}
-      />
-    </div>
-
+    <>
+      <div className="flex justify-end items-center gap-2">
+        <Searchbar
+          className="w-[300px] py-1 px-3 rounded-md bg-white"
+          onChange={handleSearch}
+        />
+        <FastCreateBookmarkButton />
+      </div>
+    </>
   );
 }
