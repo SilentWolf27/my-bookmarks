1b962be79be3898fecee0ef396e0d950209103c7
Pedro David <97pedro@live.com.mx>
2025-02-14 07:26:31 -0600

    Improve BookmarkNavItem text overflow and styling

--- src/bookmarks/components/BookmarkNavItem.tsx
+++ src/bookmarks/components/BookmarkNavItem.tsx
@@ -10,8 +10,8 @@ export default function BookmarkNavItem({ bookmark, className }: Props) {
   return (
     <Link
       href={`/colecciones/${bookmark.collection_id}/marcadores/${bookmark.id}`}
-      className={`w-full text-sm font-normal hover:bg-gray-200 transition-[background-color] duration-250 ${className}`}>
-      <h1>{bookmark.title || bookmark.url}</h1>
+      className={`w-full text-sm font-normal hover:bg-gray-200 transition-[background-color] duration-250 overflow-hidden max-w-full whitespace-nowrap text-ellipsis ${className}`}>
+      {bookmark.title || bookmark.url}
     </Link>
   );
 }
