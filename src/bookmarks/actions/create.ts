7b10b3173b05f0d640b101282cb1a7679d354308
Pedro David <97pedro@live.com.mx>
2025-02-26 07:38:39 -0600

    refactor: update bookmark creation schema and action

--- src/bookmarks/actions/create.ts
+++ src/bookmarks/actions/create.ts
@@ -4,19 +4,20 @@ import { createClient } from "@/supabase/clients/server";
 import { buildErrorFromSupabase } from "@/supabase/errors/supabase";
 import { extractOpenGraphMetadata } from "@/utils/opengraph/extractMetadata";
 import { revalidatePath } from "next/cache";
+import { CreateBookmarkFormValues } from "../schemas";
 
-export async function createBookmark(url: string, collectionId?: string) {
-  if (!url || !url.startsWith("http")) throw new Error("Invalid URL");
+export async function createBookmark(data: CreateBookmarkFormValues) {
+  if (!data.url || !data.url.startsWith("http")) throw new Error("Invalid URL");
 
-  const metadata = await extractOpenGraphMetadata(url);
+  const metadata = await extractOpenGraphMetadata(data.url);
 
   const supabase = await createClient();
 
   const { error } = await supabase.from("bookmarks").insert({
-    url,
+    url: data.url,
     title: metadata.title,
     image: metadata.image,
-    collection_id: collectionId,
+    collection_id: data.collectionId,
   });
 
   if (error) buildErrorFromSupabase(error);
--- src/collections/schemas/index.ts
+++ src/collections/schemas/index.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const CreateCollectionSchema = z.object({
   name: z.string().nonempty({ message: "El nombre es requerido" }),
+  parentId: z.string().optional(),
 });
 
 export type CreateCollectionFormValues = z.infer<typeof CreateCollectionSchema>;
