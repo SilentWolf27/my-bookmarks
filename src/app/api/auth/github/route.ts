a86842aae2119f39f25e5486492005a067f5bc16
Pedro David <97pedro@live.com.mx>
2025-02-27 10:14:51 -0600

    debug: add console log for GitHub auth route search parameters

--- src/app/api/auth/github/route.ts
+++ src/app/api/auth/github/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
+  console.log("🚀 ~ GET ~ searchParams:", searchParams);
 
   const code = searchParams.get("code");
   const next = searchParams.get("next") ?? "/";
