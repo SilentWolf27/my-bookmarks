abbbedd0ab4bdd386fe1316c76633e4b9795d7e6
Pedro David <97pedro@live.com.mx>
2025-02-27 10:04:04 -0600

    style: enhance social login button with hover and transition effects

--- src/auth/components/SocialLoginForm.tsx
+++ src/auth/components/SocialLoginForm.tsx
@@ -10,7 +10,7 @@ export function SocialLoginForm({ children, action }: Props) {
     <form action={action}>
       <button
         type="submit"
-        className="w-full flex justify-center items-center gap-2 border border-zinc-300 rounded-md py-2 px-4">
+        className="w-full flex justify-center items-center gap-2 border border-zinc-300 rounded-md py-2 px-4 cursor-pointer hover:bg-zinc-50 transition-[background-color] duration-200">
         {children}
       </button>
     </form>
